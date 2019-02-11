6eadc5f4e17fdbc0fa0aab2690abee26bed7c707
mattshardman <mattshardman@gmail.com>
2019-02-11 10:32:46 +0000

    create other header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Header.css";
 import ImageThumbnail from "./ImageThumbnail";
+import HeaderTitle from "./HeaderTitle";
+import HeaderContent from "./HeaderContent";
 
 const HeaderContainer = () => (
   <header>
     <ImageThumbnail />
+    <HeaderTitle />
+    <HeaderContent />
   </header>
 );
 
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => <div>
+    <h1>Lambda School</h1>
+    <p>@LambdaSchool</p>
+    <p>{new Date().toString()}</p>
+</div>
+
+export default HeaderTitle;
